78659f5594475eff77b3fc17aaa0166426274fe9
uguribrahimov <uguribrahimov04@gmail.com>
2025-03-27 22:11:02 +0400

    2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,40 +6,43 @@ import './Navbar.css'
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import SearchMovie from '../SearchMovie/searchMovie';
-const Navbar = () => {
 
+const Navbar = () => {
     const favoriteCounter = useSelector(store => store.favorites.favoriteMovies.length)
 
-  return (
-    <nav className="navbar">
-        <div className="left">
-            <Link to={"/"}>
-                <h1>MovieApp</h1>
-            </Link>
-        </div>
-        <div className="center">
-            <PiFilmReelFill />
-        </div>
-        <div>
-            <SearchMovie/>
-        </div>
-        <div className="right">
-            <ul>
-                <li>
-                    <Link to="/">
-                        <TiHome/>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/my-list">
-                        <FaHeart/>
-                        <div className='favorite-count'>{favoriteCounter}</div>
-                    </Link>    
-                </li>
-            </ul>
-        </div>
-    </nav>
-  )
+    return (
+        <nav className="navbar">
+            <div className="navbar__left">
+                <Link to={"/"}>
+                    <h1 className="navbar__logo">MovieApp</h1>
+                </Link>
+            </div>
+            
+            <div className="navbar__center">
+                <PiFilmReelFill className="navbar__reel-icon" />
+            </div>
+            
+            <div className="navbar__search">
+                <SearchMovie/>
+            </div>
+            
+            <div className="navbar__right">
+                <ul className="navbar__list">
+                    <li className="navbar__item">
+                        <Link to="/" className="navbar__link">
+                            <TiHome className="navbar__icon"/>
+                        </Link>
+                    </li>
+                    <li className="navbar__item navbar__favorites">
+                        <Link to="/my-list" className="navbar__link">
+                            <FaHeart className="navbar__icon"/>
+                            <span className='navbar__favorite-count'>{favoriteCounter}</span>
+                        </Link>    
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
